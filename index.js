f900e08fed7868513b70355322796ec43b21e201
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-15 18:22:08 -0700

    Fetch all pokemons.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const setup = async () => {};
+const setup = async () => {
+  $("#pokemonCards").empty();
+  let response = await axios.get(
+    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=810"
+  );
+  //   const pokemons = response.data.results;
+  // slice the pokemons
+  const pokemons = response.data.results.slice(0, 15);
+  var newList = $(`<ol></ol>`);
+  for (let i = 0; i < pokemons.length; i++) {
+    newList.append(`<li> ${pokemons[i].name}</li>`);
+  }
+  $("#pokemonCards").append(newList);
+  console.log(pokemons);
+};
 
 $(document).ready(setup);
