a8516459f2a0e6fdc2c5244c4d9d961d95516b07
Olga Zimina <olga.vic.zimina@gmail.com>
2023-05-15 19:07:29 -0700

    Display the pokemon's details in the modal.

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const setup = async () => {
   for (let i = 0; i < pokemons.length; i++) {
     let innerResponse = await axios.get(`${pokemons[i].url}`);
     let thisPokemon = innerResponse.data;
-    console.log(thisPokemon);
+    //   show modal on More btn click
     $("#pokemonCards").append(`
-      <div class="pokemonCard card"  >
+      <div class="pokemonCard card" pokemonName=${thisPokemon.name}>
         <h3>${thisPokemon.name}</h3> 
         <img src="${thisPokemon.sprites.front_default}" alt="${thisPokemon.name}"/>
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokemonModal">
@@ -20,6 +20,52 @@ const setup = async () => {
         </div>  
         `);
   }
+
+  // display pokemon details in modal
+  $("body").on("click", ".pokemonCard", async function (e) {
+    const pokemonName = $(this).attr("pokemonName");
+    console.log("pokemonName: ", pokemonName);
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const pokemonDetails = res.data;
+    console.log("res.data: ", pokemonDetails);
+    const types = pokemonDetails.types.map((type) => type.type.name);
+    console.log("types: ", types);
+    $(".modal-body").html(`
+        <div style="width:200px">
+            <img src="${
+              pokemonDetails.sprites.other["official-artwork"].front_default
+            }" alt="${pokemonDetails.name}"/>
+            <div>
+                <h3>Abilities</h3>
+                <ul>
+                    ${pokemonDetails.abilities
+                      .map((ability) => `<li>${ability.ability.name}</li>`)
+                      .join("")}
+                </ul>
+            </div>
+
+            <div>
+                <h3>Stats</h3>
+                <ul>
+                ${pokemonDetails.stats
+                  .map(
+                    (stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`
+                  )
+                  .join("")}
+                </ul>
+            </div>
+        </div>
+
+          <h3>Types</h3>
+          <ul>
+          ${types.map((type) => `<li>${type}</li>`).join("")}
+          </ul>
+      
+        `);
+    $(".modal-title").html(`<h2>${pokemonDetails.name.toUpperCase()}</h2>`);
+  });
 };
 
 $(document).ready(setup);
